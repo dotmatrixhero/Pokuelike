d13c7bb8b930d3a67447361847042c7f5a0f9d24
Brian Park <brokenazn@gmail.com>
2012-07-08 19:41:51 -0700

    roll again

--- windows_build/Pokuelike/nbproject/Makefile-variables.mk
+++ windows_build/Pokuelike/nbproject/Makefile-variables.mk
@@ -12,8 +12,8 @@ CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Debug=pokuelike
 CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/pokuelike
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=pokuelike.zip
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/pokuelike.zip
+CND_PACKAGE_NAME_Debug=pokuelike.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/pokuelike.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
