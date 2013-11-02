3fd211773988575060214655bde6b357f14763ab
Brian Park <brokenazn@gmail.com>
2013-11-01 23:40:13 -0700

    final things

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-	TCODConsole::initRoot(80, 50, "I hope this works");
-	bool endGame = false;
+        TCODConsole::initRoot(80, 50, "I hope this works");
+        bool endGame = false;
 
-	while (!endGame && !TCODConsole::isWindowClosed())
-	  {
-	    TCODConsole::flush();
-	    TCOD_key_t key = TCODConsole::waitForKeypress(true);
-	  }
-	return 0;
+        while (!endGame && !TCODConsole::isWindowClosed())
+        {
+                TCODConsole::flush();
+                TCOD_key_t key = TCODConsole::waitForKeypress(true);
+        }
+        return 0;
 }
